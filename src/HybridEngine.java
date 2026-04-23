d206d30302c243729253a19c5c55883cd147d5e7
omar <202027471@std.sci.cu.edu.eg>
2026-04-23 23:46:38 +0200

    implement hybrid Engine

---
+++ src/HybridEngine.java
@@ -0,0 +1,39 @@
+public class HybridEngine implements Engine {
+    private final GasEngine gasEngine = new GasEngine();
+    private final   ElectricEngine electricEngine = new ElectricEngine();
+    private Engine operatingEngine;
+
+
+    @Override
+    public void start() {
+        System.out.println("Engine started");
+        operatingEngine = electricEngine;
+        operatingEngine.start();
+    }
+
+    @Override
+    public void stop() {
+        operatingEngine.stop();
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        Engine selectedEngine;
+        if (speed<=50)  selectedEngine = electricEngine;
+        else selectedEngine=gasEngine;
+
+        //make sure the car started
+        if (operatingEngine==null){
+            operatingEngine=selectedEngine;
+            operatingEngine.start();
+        }
+        //change engines if needed
+        if(selectedEngine!=operatingEngine){
+            operatingEngine.stop();
+            selectedEngine.start();
+            operatingEngine=selectedEngine;
+        }
+        operatingEngine.setSpeed(speed);
+    }
+
+}

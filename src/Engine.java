7a58a66db035deb5187c71012b39e03d68ac855a
omar <202027471@std.sci.cu.edu.eg>
2026-04-24 03:06:38 +0300

    change engine to abstract class to add internalSpeed attribute, increase and decrease functions

--- src/Engine.java
+++ src/Engine.java
@@ -1,5 +1,23 @@
-public interface Engine {
-    void start();
-    void stop();
-    void setSpeed(int speed);
+public abstract class Engine {
+    private int internalSpeed = 0;
+
+    abstract void start();
+
+    abstract void stop();
+
+    int getInternalSpeed() {
+        return internalSpeed;
+    }
+
+    abstract void setInternalSpeed(int speed);
+
+    void increase() {
+        internalSpeed++;
+    }
+
+    public void decrease() {
+        if (internalSpeed > 0)
+            internalSpeed--;
+    }
+
 }

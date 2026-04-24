45ea3e8c96475ada70b617b942c4805c87805d73
omar <202027471@std.sci.cu.edu.eg>
2026-04-24 03:09:40 +0300

    update stop and setInternalSpeed methods in ElectricEngine

--- src/ElectricEngine.java
+++ src/ElectricEngine.java
@@ -1,4 +1,4 @@
-public class ElectricEngine implements Engine {
+public class ElectricEngine extends Engine {
 
     @Override
     public void start() {
@@ -7,12 +7,22 @@ public class ElectricEngine implements Engine {
 
     @Override
     public void stop() {
+        this.setInternalSpeed(0);
         System.out.println("Electric Engine stopped");
     }
 
     @Override
-    public void setSpeed(int speed) {
-        System.out.println("Electric Engine current speed " + speed);
+    public void setInternalSpeed(int speed) {
+        while (this.getInternalSpeed() < speed) {
+            this.increase();
+            System.out.println("Electric Engine current speed " + this.getInternalSpeed());
+        }
+        while (this.getInternalSpeed() > speed) {
+            this.decrease();
+            System.out.println("Electric Engine current speed " + this.getInternalSpeed());
+        }
+
+
     }
 
 }
